5c260172e1d40960c568f3b241a9e4ea3f4d541d
lichunlong0630 <lichunlong0630@163.com>
2020-01-22 23:22:13 +0800

    添加基础框架

--- vendor/lichunlong0630/phptools/README.md
+++
@@ -1,4 +0,0 @@
-# phptools
-仅用于学习和研究
-## 1）基于Thinkphp6 的 Auth，基础来源于5ini99的think-auth，略做修改。
-## 2）基于Thinkphp6 的 Jump，基础来源于liliuwei/thinkphp-jump，略做修改。
--- vendor/lichunlong0630/phptools/src/config/jump.php
+++
@@ -1,7 +0,0 @@
-<?php
-return[
-    // 默认跳转页面对应的模板文件
-    'dispatch_success_tmpl' => app()->getRootPath().'/vendor/lichunlong0630/phptools/src/tpl/dispatch_jump.tpl',
-    'dispatch_error_tmpl'   => app()->getRootPath().'/vendor/lichunlong0630/phptools/src/tpl/dispatch_jump.tpl',
-    'dispatch_info_tmpl'    => app()->getRootPath().'/vendor/lichunlong0630/phptools/src/tpl/dispatch_jump.tpl'
-];
